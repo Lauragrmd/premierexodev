fc715d405843d7fe1c831c26472c8f2c64ededd3
Lauragrmd <grmdlaura@gmail.com>
2020-10-12 14:39:20 +0200

    premierexo

---
+++ Projetuncsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projetuncsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 4;
+
+            string add = "Addition";
+            string sub = "Soustraction";
+
+         
+            Console.WriteLine("L'" + add + " entre " + a + " et " + b + " vaut " + (a+b));
+            Console.WriteLine("La" + sub + " entre " + a + )
+            Console.ReadLine();
+        }
+    }
+}
